66fa44c97d928218843f08241c93c3d399f502c9
Bodix <bodix321@gmail.com>
2021-03-26 14:49:37 +0200

    Improve attributions

--- Scripts/Runtime/Components/Animations/InOutBehaviour.cs
+++ Scripts/Runtime/Components/Animations/InOutBehaviour.cs
@@ -1,4 +1,5 @@
-﻿// Copyright © 2020 Bogdan Nikolayev <bodix321@gmail.com>
+﻿// Evolunity for Unity
+// Copyright © 2020 Bogdan Nikolayev <bodix321@gmail.com>
 // All Rights Reserved
 
 using System;
