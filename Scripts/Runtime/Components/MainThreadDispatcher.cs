ca38595d4d14779acde82099390e1da822f6cf4c
Bodix <bodix321@gmail.com>
2021-04-30 18:15:56 +0300

    Improve `MainThreadDispatcher`

--- Scripts/Runtime/Components/MainThreadDispatcher.cs
+++ Scripts/Runtime/Components/MainThreadDispatcher.cs
@@ -1,22 +1,30 @@
-﻿/*
-Copyright 2015 Pim de Witte All Rights Reserved.
+﻿// Evolunity for Unity
+// Copyright © 2020 Bogdan Nikolayev <bodix321@gmail.com>
+// All Rights Reserved
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+// Updates by Bogdan Nikolayev:
+// 
+// 1. Previous singleton code changed to inheritance from SingletonBehaviour. 
+// 2. Removed redundant wrapping to coroutine.
+// 3. Added to component menu.
+// 4. Refactored namings and access modifiers.
+// 5. Improved formatting and XML comments.
 
-    http://www.apache.org/licenses/LICENSE-2.0
+/*
+    Copyright 2015 Pim de Witte All Rights Reserved.
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 */
-
-// Changed formatting
-// Changed class name and namespace
-// Refactored namings
 
 using System;
 using System.Collections;
@@ -26,12 +34,16 @@ using UnityEngine;
 
 namespace Evolutex.Evolunity.Components
 {
-    /// Author: Pim de Witte (pimdewitte.com) and contributors, https://github.com/PimDeWitte/UnityMainThreadDispatcher
+
     /// <summary>
+    /// Author: Pim de Witte (pimdewitte.com) and contributors, https://github.com/PimDeWitte/UnityMainThreadDispatcher
+    /// <para />
+    /// <para />
     /// A thread-safe class which holds a queue with actions to execute on the next Update() method. It can be used to make calls to the main thread for
-    /// things such as UI Manipulation in Unity. It was developed for use in combination with the Firebase Unity plugin, which uses separate threads for event handling
+    /// things such as UI manipulation in Unity. It was developed for use in combination with the Firebase Unity plugin, which uses separate threads for event handling
     /// </summary>
-    public class UnityMainThreadDispatcher : MonoBehaviour
+    [AddComponentMenu("Evolunity/Main Thread Dispatcher")]
+    public class MainThreadDispatcher : SingletonBehaviour<MainThreadDispatcher>
     {
         private static readonly Queue<Action> actionQueue = new Queue<Action>();
 
@@ -48,17 +60,18 @@ namespace Evolutex.Evolunity.Components
         /// <param name="action">function that will be executed from the main thread.</param>
         public void Enqueue(Action action)
         {
-            Enqueue(ActionWrapper(action));
+            lock (actionQueue)
+                actionQueue.Enqueue(action);
         }
 
         /// <summary>
         /// Locks the queue and adds the IEnumerator to the queue
         /// </summary>
-        /// <param name="enumerator">IEnumerator function that will be executed from the main thread.</param>
-        public void Enqueue(IEnumerator enumerator)
+        /// <param name="routine">IEnumerator function that will be executed from the main thread.</param>
+        public void EnqueueStartCoroutine(IEnumerator routine)
         {
-            lock (actionQueue) 
-                actionQueue.Enqueue(() => StartCoroutine(enumerator));
+            lock (actionQueue)
+                actionQueue.Enqueue(() => StartCoroutine(routine));
         }
 
         /// <summary>
@@ -68,29 +81,23 @@ namespace Evolutex.Evolunity.Components
         /// <returns>A Task that can be awaited until the action completes</returns>
         public Task EnqueueAsync(Action action)
         {
-            var tcs = new TaskCompletionSource<bool>();
+            TaskCompletionSource<bool> taskCompletionSource = new TaskCompletionSource<bool>();
 
-            void WrappedAction()
+            Enqueue(() =>
             {
                 try
                 {
-                    action();
-                    tcs.TrySetResult(true);
+                    action.Invoke();
+
+                    taskCompletionSource.TrySetResult(true);
                 }
                 catch (Exception ex)
                 {
-                    tcs.TrySetException(ex);
+                    taskCompletionSource.TrySetException(ex);
                 }
-            }
+            });
 
-            Enqueue(ActionWrapper(WrappedAction));
-            return tcs.Task;
-        }
-
-        private IEnumerator ActionWrapper(Action action)
-        {
-            action();
-            yield return null;
+            return taskCompletionSource.Task;
         }
     }
 }
