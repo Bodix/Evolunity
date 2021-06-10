99d3005379debdb6c419d87fa66bf4b05b38ad7e
Bodix <bodix321@gmail.com>
2021-06-10 17:57:59 +0300

    Add extensions and wrapper for coroutines

---
+++ Scripts/Runtime/Extensions/Unity/UnityWebRequestExtensions.cs
@@ -0,0 +1,41 @@
+// Evolunity for Unity
+// Copyright © 2020 Bogdan Nikolayev <bodix321@gmail.com>
+// All Rights Reserved
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using Evolutex.Evolunity.Utilities;
+
+namespace Evolutex.Evolunity.Extensions
+{
+    public static class UnityWebRequestExtensions
+    {
+        public delegate void ResponseHandler(UnityWebRequest response);
+
+        public static CoroutineWrapper WaitForResponseInWrappedCoroutine(
+            this UnityWebRequestAsyncOperation asyncOperation, MonoBehaviour monoBehaviour, ResponseHandler onComplete)
+        {
+            return monoBehaviour.StartWrappedCoroutine(WaitForResponse(asyncOperation, onComplete));
+        }
+
+        public static Coroutine WaitForResponseInCoroutine(
+            this UnityWebRequestAsyncOperation asyncOperation, MonoBehaviour monoBehaviour, ResponseHandler onComplete)
+        {
+            return monoBehaviour.StartCoroutine(WaitForResponse(asyncOperation, onComplete));
+        }
+
+        private static IEnumerator WaitForResponse(UnityWebRequestAsyncOperation asyncOperation,
+            ResponseHandler onComplete)
+        {
+            yield return asyncOperation;
+
+            onComplete?.Invoke(asyncOperation.webRequest);
+        }
+
+        public static bool IsError(this UnityWebRequest webRequest)
+        {
+            return webRequest.isNetworkError || webRequest.isHttpError;
+        }
+    }
+}
---
+++ Scripts/Runtime/Utilities/CoroutineWrapper.cs
@@ -0,0 +1,57 @@
+// Evolunity for Unity
+// Copyright © 2020 Bogdan Nikolayev <bodix321@gmail.com>
+// All Rights Reserved
+
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Evolutex.Evolunity.Utilities
+{
+    // TODO:
+    // 1. Test stopping null coroutine.
+    // 2. Add completion callback.
+    // 3. Implement IDisposable and check the disposing.
+
+    public class CoroutineWrapper
+    {
+        private readonly IEnumerator routine;
+        
+        public static implicit operator Coroutine(CoroutineWrapper wrapper) => wrapper.Coroutine;
+
+        public MonoBehaviour Owner { get; }
+        public Coroutine Coroutine { get; private set; }
+        public bool IsRunning => Coroutine != null;
+
+        public CoroutineWrapper(MonoBehaviour owner, IEnumerator routine)
+        {
+            this.routine = routine;
+            
+            Owner = owner;
+        }
+
+        public CoroutineWrapper Start()
+        {
+            if (IsRunning)
+                throw new InvalidOperationException("The coroutine is already started");
+            
+            Coroutine = Owner.StartCoroutine(Process());
+
+            return this;
+        }
+
+        public void Stop()
+        {
+            Owner.StopCoroutine(Coroutine);
+
+            Coroutine = null;
+        }
+
+        private IEnumerator Process()
+        {
+            yield return routine;
+
+            Coroutine = null;
+        }
+    }
+}
