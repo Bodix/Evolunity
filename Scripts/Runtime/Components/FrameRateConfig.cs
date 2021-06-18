2c4898b8963423006d328ba30ff7cef681ed0b1c
Bodix <bodix321@gmail.com>
2021-06-18 21:17:37 +0300

    Add `FrameRateConfig`

---
+++ Scripts/Runtime/Components/FrameRateConfig.cs
@@ -0,0 +1,24 @@
+﻿// Evolunity for Unity
+// Copyright © 2020 Bogdan Nikolayev <bodix321@gmail.com>
+// All Rights Reserved
+
+using UnityEngine;
+
+namespace Evolutex.Evolunity.Components
+{
+    [AddComponentMenu("Evolunity/Frame Rate Config")]
+    [DisallowMultipleComponent]
+    public class FrameRateConfig : MonoBehaviour
+    {
+        [SerializeField]
+        private int targetFrameRate = 120;
+        [SerializeField]
+        private int vSyncCount = 0;
+
+        private void Start()
+        {
+            QualitySettings.vSyncCount = vSyncCount;
+            Application.targetFrameRate = targetFrameRate;
+        }
+    }
+}
