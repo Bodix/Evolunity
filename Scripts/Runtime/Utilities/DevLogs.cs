9b55ae0008e4daf900bfe0d1c20ced7353c5a0c4
Bodix <bodix321@gmail.com>
2023-10-02 11:28:27 +0300

    Add `DevLogs`

---
+++ Scripts/Runtime/Utilities/DevLogs.cs
@@ -0,0 +1,131 @@
+﻿#if UNITY_EDITOR || DEVELOPMENT_BUILD
+#define DEV_LOGS
+#else
+#undef DEV_LOGS
+#endif
+
+using System;
+using UnityEngine;
+
+namespace _WIP
+{
+    /// <summary>
+    /// Works only when "Development Build" option is enabled (in "Build Settings") or in Unity Editor.
+    /// </summary>
+    public static class DevLogs
+    {
+        private const string DefineSymbol = "DEV_LOGS";
+
+        [System.Diagnostics.Conditional(DefineSymbol)]
+        public static void Log(object message)
+        {
+            Debug.Log(message);
+        }
+
+        [System.Diagnostics.Conditional(DefineSymbol)]
+        public static void Log(object message, UnityEngine.Object context)
+        {
+            Debug.Log(message, context);
+        }
+
+        [System.Diagnostics.Conditional(DefineSymbol)]
+        public static void LogFormat(string format, params object[] args)
+        {
+            Debug.LogFormat(format, args);
+        }
+
+        [System.Diagnostics.Conditional(DefineSymbol)]
+        public static void LogFormat(UnityEngine.Object context, string format, params object[] args)
+        {
+            Debug.LogFormat(context, format, args);
+        }
+
+
+        [System.Diagnostics.Conditional(DefineSymbol)]
+        public static void LogWarning(object message)
+        {
+            Debug.LogWarning(message);
+        }
+
+        [System.Diagnostics.Conditional(DefineSymbol)]
+        public static void LogWarning(object message, UnityEngine.Object context)
+        {
+            Debug.LogWarning(message, context);
+        }
+
+        [System.Diagnostics.Conditional(DefineSymbol)]
+        public static void LogWarningFormat(string format, params object[] args)
+        {
+            Debug.LogWarningFormat(format, args);
+        }
+
+        [System.Diagnostics.Conditional(DefineSymbol)]
+        public static void LogWarningFormat(UnityEngine.Object context, string format, params object[] args)
+        {
+            Debug.LogWarningFormat(context, format, args);
+        }
+
+
+        [System.Diagnostics.Conditional(DefineSymbol)]
+        public static void LogError(object message)
+        {
+            Debug.LogError(message);
+        }
+
+        [System.Diagnostics.Conditional(DefineSymbol)]
+        public static void LogError(object message, UnityEngine.Object context)
+        {
+            Debug.LogError(message, context);
+        }
+
+        [System.Diagnostics.Conditional(DefineSymbol)]
+        public static void LogErrorFormat(string format, params object[] args)
+        {
+            Debug.LogErrorFormat(format, args);
+        }
+
+        [System.Diagnostics.Conditional(DefineSymbol)]
+        public static void LogErrorFormat(UnityEngine.Object context, string format, params object[] args)
+        {
+            Debug.LogErrorFormat(context, format, args);
+        }
+
+
+        [System.Diagnostics.Conditional(DefineSymbol)]
+        public static void LogAssertion(object message)
+        {
+            Debug.LogAssertion(message);
+        }
+
+        [System.Diagnostics.Conditional(DefineSymbol)]
+        public static void LogAssertion(object message, UnityEngine.Object context)
+        {
+            Debug.LogAssertion(message, context);
+        }
+
+        [System.Diagnostics.Conditional(DefineSymbol)]
+        public static void LogAssertionFormat(string format, params object[] args)
+        {
+            Debug.LogAssertionFormat(format, args);
+        }
+
+        [System.Diagnostics.Conditional(DefineSymbol)]
+        public static void LogAssertionFormat(UnityEngine.Object context, string format, params object[] args)
+        {
+            Debug.LogAssertionFormat(context, format, args);
+        }
+
+
+        [System.Diagnostics.Conditional(DefineSymbol)]
+        public static void LogException(Exception exception)
+        {
+            Debug.LogException(exception);
+        }
+
+        [System.Diagnostics.Conditional(DefineSymbol)]
+        public static void LogException(Exception exception, UnityEngine.Object context)
+        {
+            Debug.LogException(exception, context);
+        }
+    }
+}
