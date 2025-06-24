ddb66dc1b99fa9537f3b78ee5a2bca48299b97d2
Bodix <bodix321@gmail.com>
2025-06-24 22:19:22 +0300

    Improve `AutoExpandHierarchy`

--- Scripts/Editor/Utilities/AutoExpandHierarchy.cs
+++ Scripts/Editor/Utilities/AutoExpandHierarchy.cs
@@ -10,36 +10,37 @@ using UnityEditor.SceneManagement;
 namespace Evolutex.Evolunity.Editor.Utilities
 {
     // TODO: Add UI for adding GameObject's to expand.
-    
+
+    // Use it as follows:
+    // [InitializeOnLoad]
+    // public static class EditorBootstrap
+    // {
+    //     static EditorBootstrap()
+    //     {
+    //         AutoExpandHierarchy.GameObjectsToExpand.Add("Environment");  
+    //         AutoExpandHierarchy.GameObjectsToExpand.Add("Game");
+    //         AutoExpandHierarchy.GameObjectsToExpand.Add("UI");
+    //     }
+    // }
+
     [InitializeOnLoad]
     public static class AutoExpandHierarchy
     {
-        private static readonly List<GameObject> gameObjectsBuffer = new List<GameObject>();
-
         static AutoExpandHierarchy()
         {
-            EditorSceneManager.sceneOpened += (scene, mode) =>
-            {
-                GameObjectsToExpand.ForEach(name =>
-                {
-                    GameObject gameObject = GameObject.Find(name);
-                    if (gameObject)
-                        gameObjectsBuffer.Add(gameObject);
-                });
-
-                foreach (GameObject gameObject in gameObjectsBuffer)
-                    SceneHierarchy.SetExpanded(gameObject, true);
-
-                gameObjectsBuffer.Clear();
-            };
+            EditorSceneManager.sceneOpened += (scene, mode) => ExpandObjects();
         }
-        
-        public static List<string> GameObjectsToExpand { get; } = new List<string>
+
+        private static void ExpandObjects()
         {
-            "Player",
-            "Logic",
-            "Environment",
-            "Test"
-        };
+            GameObjectsToExpand.ForEach(name =>
+            {
+                GameObject gameObject = GameObject.Find(name);
+                if (gameObject)
+                    SceneHierarchy.SetExpanded(gameObject, true);
+            });
+        }
+
+        public static List<string> GameObjectsToExpand { get; } = new List<string>();
     }
 }
