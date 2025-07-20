9dd35edefb9412dec31693115d20a128601b5818
Bodix <bodix321@gmail.com>
2025-07-21 02:04:37 +0300

    Improve functional extensions

--- Scripts/Runtime/Extensions/System/FunctionalExtensions.cs
+++ Scripts/Runtime/Extensions/System/FunctionalExtensions.cs
@@ -8,9 +8,18 @@ namespace Evolutex.Evolunity.Extensions
 {
     public static class FunctionalExtensions
     {
-        public static T With<T>(this T value, Action<T> action)
+        public static T With<T>(this T value, Action<T> action) where T : class
         {
             action.Invoke(value);
+
+            return value;
+        }
+
+        public delegate void ActionWithRef<T>(ref T obj);
+
+        public static T WithValue<T>(this T value, ActionWithRef<T> action) where T : struct
+        {
+            action.Invoke(ref value);
 
             return value;
         }
