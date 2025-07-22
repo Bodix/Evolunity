dba065a6035c70408afd7b678b040134d615cba5
Bodix <bodix321@gmail.com>
2025-07-22 07:54:16 +0300

    Add `DisableButtonAfterClick` component

---
+++ Scripts/Runtime/Components/UI/DisableButtonAfterClick.cs
@@ -0,0 +1,20 @@
+﻿// Evolunity for Unity
+// Copyright © 2020 Bogdan Nikolayev <bodix321@gmail.com>
+// All Rights Reserved
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Evolutex.Evolunity.Components.UI
+{
+    [RequireComponent(typeof(Button))]
+    public class DisableButtonAfterClick : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Button button = GetComponent<Button>();
+
+            button.onClick.AddListener(() => button.interactable = false);
+        }
+    }
+}
