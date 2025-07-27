7242e2e0a2c594fdc60f99cb9692449ed1cc58ff
Bodix <bodix321@gmail.com>
2025-07-27 18:56:15 +0300

    Update utility

--- Scripts/Runtime/Utilities/Validate.cs
+++ Scripts/Runtime/Utilities/Validate.cs
@@ -2,6 +2,9 @@
 // Copyright © 2020 Bogdan Nikolayev <bodix321@gmail.com>
 // All Rights Reserved
 
+using System;
+using System.Net;
+using Evolutex.Evolunity.Extensions;
 using UnityEngine;
 
 namespace Evolutex.Evolunity.Utilities
@@ -9,12 +12,27 @@ namespace Evolutex.Evolunity.Utilities
     // TODO:
     // 1. Password (regex).
     // 2. Email (regex).
-    
+
     public static class Validate
     {
         public static bool InternetConnection()
         {
             return Application.internetReachability != NetworkReachability.NotReachable;
+        }
+
+        /// <summary>
+        /// Avoid use UriKind.Relative or UriKind.RelativeOrAbsolute because most of the strings are valid URLs.
+        /// For example, "asdasd", "/", and "?????" are valid in this case.
+        /// </summary>
+        public static bool Url(string url, UriKind urlKind = UriKind.Absolute)
+        {
+            return !url.IsEmpty() // Uri.IsWellFormedUriString with UriKind.Relative or UriKind.RelativeOrAbsolute returns "true" for empty string ("").
+                && (Uri.IsWellFormedUriString(url, urlKind) || IPAddress(url));
+        }
+
+        public static bool IPAddress(string ipAddress)
+        {
+            return System.Net.IPAddress.TryParse(ipAddress, out IPAddress _);
         }
     }
 }
