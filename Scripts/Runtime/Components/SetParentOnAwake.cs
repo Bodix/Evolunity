25ef9a770a7b3332364ab7562c85f6452c0caa60
Bodix <bodix321@gmail.com>
2025-07-27 05:58:29 +0300

    Add new component

---
+++ Scripts/Runtime/Components/SetParentOnAwake.cs
@@ -0,0 +1,20 @@
+﻿// Evolunity for Unity
+// Copyright © 2020 Bogdan Nikolayev <bodix321@gmail.com>
+// All Rights Reserved
+
+using UnityEngine;
+
+namespace Evolutex.Evolunity.Components
+{
+    [AddComponentMenu("Evolunity/Set Parent On Awake")]
+    public class SetParentOnAwake : MonoBehaviour
+    {
+        public Transform NewParent;
+        public bool WorldPositionStays = true;
+
+        private void Awake()
+        {
+            transform.SetParent(NewParent, WorldPositionStays);
+        }
+    }
+}
