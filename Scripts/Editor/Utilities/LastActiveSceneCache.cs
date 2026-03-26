57537a78ae723c0267ee88f7de1319c3188b7b07
Bodix <bodix321@gmail.com>
2026-03-26 07:00:42 +0200

    Add `LastActiveSceneCache` script. Refactoring

---
+++ Scripts/Editor/Utilities/LastActiveSceneCache.cs
@@ -0,0 +1,26 @@
+// Evolunity for Unity
+// Copyright © 2020 Bogdan Nikolayev <bodix321@gmail.com>
+// All Rights Reserved
+
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+namespace Evolutex.Evolunity.Editor.Utilities
+{
+	[InitializeOnLoad]
+	public static class LastActiveSceneCache
+	{
+		public const string LastActiveScenePathKey = "LastActiveScenePath";
+
+		static LastActiveSceneCache()
+		{
+			EditorApplication.playModeStateChanged += CacheLastActiveScene;
+		}
+
+		private static void CacheLastActiveScene(PlayModeStateChange state)
+		{
+			if (state == PlayModeStateChange.ExitingEditMode)
+				EditorPrefs.SetString(LastActiveScenePathKey, SceneManager.GetActiveScene().path);
+		}
+	}
+}
