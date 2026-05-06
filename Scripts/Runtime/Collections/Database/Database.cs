42f1abc8bcf379ae93b5f42d8c4fe44ada4b583f
Bodix <bodix321@gmail.com>
2026-05-06 06:04:33 +0300

    Add `Database` scriptable object

---
+++ Scripts/Runtime/Collections/Database/Database.cs
@@ -0,0 +1,46 @@
+﻿// Evolunity for Unity
+// Copyright © 2020 Bogdan Nikolayev <bodix321@gmail.com>
+// All Rights Reserved
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Evolutex.Evolunity.Collections
+{
+	public abstract class Database<T> : ScriptableObject where T : DatabaseEntry
+	{
+		[SerializeField]
+		protected List<T> Items = new List<T>();
+
+		private Dictionary<string, T> _itemsById;
+
+		public void Initialize()
+		{
+			_itemsById = new Dictionary<string, T>(Items.Count);
+
+			foreach (T item in Items)
+			{
+				if (item == null)
+					continue;
+
+				if (_itemsById.ContainsKey(item.Id))
+					Debug.LogError($"[{nameof(Database<T>)}] Duplicate ID found: {item.Id} on object {item.name}.");
+				else
+					_itemsById.Add(item.Id, item);
+			}
+		}
+
+		public T GetById(string id)
+		{
+			if (_itemsById == null)
+				Initialize();
+
+			// ReSharper disable once PossibleNullReferenceException
+			if (_itemsById.TryGetValue(id, out T result))
+				return result;
+
+			Debug.LogWarning($"[{nameof(Database<T>)}] Item with ID {id} not found!");
+			return null;
+		}
+	}
+}
---
+++ Scripts/Runtime/Collections/Database/DatabaseEntry.cs
@@ -0,0 +1,42 @@
+﻿// Evolunity for Unity
+// Copyright © 2020 Bogdan Nikolayev <bodix321@gmail.com>
+// All Rights Reserved
+
+using System.IO;
+using NaughtyAttributes;
+using UnityEditor;
+using UnityEngine;
+
+namespace Evolutex.Evolunity.Collections
+{
+	public abstract class DatabaseEntry : ScriptableObject
+	{
+		[Tooltip("Unique string ID in 'namespace:name' format")]
+		[SerializeField, ReadOnly] private string _id;
+
+		public string Id => _id;
+
+#if UNITY_EDITOR
+		protected virtual void OnValidate()
+		{
+			string expectedId = name.Replace(" ", "_").ToLower();
+			string assetPath = AssetDatabase.GetAssetPath(this);
+
+			if (!string.IsNullOrEmpty(assetPath))
+			{
+				// ReSharper disable once AssignNullToNotNullAttribute
+				string parentFolder = new DirectoryInfo(Path.GetDirectoryName(assetPath)).Name.ToLower();
+
+				expectedId = $"{parentFolder}:{expectedId}";
+			}
+
+			if (_id != expectedId)
+			{
+				_id = expectedId;
+
+				EditorUtility.SetDirty(this);
+			}
+		}
+#endif
+	}
+}
