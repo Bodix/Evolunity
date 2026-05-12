99847b28ad18bcc671eec4e3979eba97f906a043
Bodix <bodix321@gmail.com>
2026-05-13 02:10:04 +0300

    Add orientation components

---
+++ Scripts/Runtime/Components/OrientationChangeFov.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Evolutex.Evolunity.Components
+{
+	public class OrientationChangeFov : MonoBehaviour
+	{
+		public Camera Camera;
+		public float PortraitFov = 90;
+		public float LandscapeFov = 60;
+
+		[SerializeField]
+		private OrientationChangeListener _orientationListener;
+
+		private void Awake()
+		{
+			_orientationListener.OrientationChanged.AddListener(ChangeCameraFov);
+		}
+
+		private void OnDestroy()
+		{
+			_orientationListener.OrientationChanged.RemoveListener(ChangeCameraFov);
+		}
+
+		private void ChangeCameraFov(bool isLandscape)
+		{
+			Camera.fieldOfView = isLandscape ? LandscapeFov : PortraitFov;
+		}
+	}
+}
---
+++ Scripts/Runtime/Components/OrientationChangeListener.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Evolutex.Evolunity.Components
+{
+	public class OrientationChangeListener : MonoBehaviour
+	{
+		[System.Serializable]
+		public class OrientationChangeEvent : UnityEvent<bool>
+		{
+		}
+
+		/// <summary>
+		/// Event fires when orientation changes. True = Landscape, False = Portrait.
+		/// </summary>
+		public OrientationChangeEvent OrientationChanged;
+
+		private bool _isLandscape;
+		private Vector2 _lastResolution;
+
+		private void Start()
+		{
+			CheckOrientation();
+		}
+
+		private void Update()
+		{
+			// Check if resolution changed (more reliable than Screen.orientation on WebGL).
+			if (_lastResolution.x != Screen.width || _lastResolution.y != Screen.height)
+				CheckOrientation();
+		}
+
+		private void CheckOrientation()
+		{
+			_lastResolution = new Vector2(Screen.width, Screen.height);
+			bool isLandscape = Screen.width > Screen.height;
+
+			if (_isLandscape != isLandscape)
+			{
+				_isLandscape = isLandscape;
+				
+				OrientationChanged.Invoke(_isLandscape);
+			}
+		}
+	}
+}
