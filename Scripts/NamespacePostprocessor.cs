a5634d62519af58e1fd739d2154d0d3df6ccf444
Bodix <bodix321@gmail.com>
2026-05-16 05:12:41 +0300

    Add namespace postprocessor

---
+++ Scripts/NamespacePostprocessor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+
+namespace Evolutex.Evolunity.Editor
+{
+	public class NamespacePostprocessor : AssetPostprocessor
+	{
+		public static string OnGeneratedCSProject(string path, string content)
+		{
+			if (path.EndsWith("Evolunity.Runtime.csproj") || path.EndsWith("Evolunity.Editor.csproj"))
+			{
+				string rootNamespaceTag = "<RootNamespace>Bodix</RootNamespace>";
+
+				content = content.Replace("<RootNamespace></RootNamespace>", rootNamespaceTag);
+			}
+
+			return content;
+		}
+	}
+}
