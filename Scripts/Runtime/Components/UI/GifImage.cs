48de3b88251556b09a133b335db9018255b4fac3
Bodix <bodix321@gmail.com>
2026-05-16 03:51:19 +0300

    Improve `GifImage`

--- Scripts/Runtime/Components/UI/GifImage.cs
+++ Scripts/Runtime/Components/UI/GifImage.cs
@@ -2,18 +2,16 @@
 // Copyright © 2020 Bogdan Nikolayev <bodix321@gmail.com>
 // All Rights Reserved
 
-using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Evolutex.Evolunity.Components.UI
 {
-	// TODO: Add OnEnable() and OnDisable() logic.
-
 	[AddComponentMenu("Evolunity/UI/GIF Image")]
 	[RequireComponent(typeof(Image))]
 	public class GifImage : MonoBehaviour
 	{
+		[Min(0.1f)]
 		public float FramesPerSecond = 30f;
 
 		[SerializeField]
@@ -24,9 +22,12 @@ namespace Evolutex.Evolunity.Components.UI
 		private Image image;
 
 		public Sprite[] Sprites => sprites;
-		public bool IsPlaying { get; private set; } = true;
-		public bool IsContainsSprites => Sprites.Length != 0;
+		public bool IsPlaying { get; private set; }
+		public bool IsContainsSprites => sprites != null && sprites.Length > 0;
 		private bool IsInitialized => image;
+
+		private float _frameTimer;
+		private int _currentFrameIndex;
 
 		public void OnValidate()
 		{
@@ -40,31 +41,56 @@ namespace Evolutex.Evolunity.Components.UI
 
 		private void Start()
 		{
-			if (!IsContainsSprites)
-				IsPlaying = false;
+			if (IsContainsSprites)
+				Play();
+		}
+
+		private void OnEnable()
+		{
+			// If it was playing before being disabled, resume it visually.
+			if (IsPlaying && IsContainsSprites)
+				image.sprite = sprites[_currentFrameIndex];
+		}
+
+		private void OnDisable()
+		{
+			// Optional: We don't change IsPlaying flag here so it resumes automatically in OnEnable.
+			// But we stop the Update logic natively because Unity doesn't call Update on disabled components.
 		}
 
 		private void Update()
 		{
-			if (IsPlaying)
-			{
-				int index = (int)(Time.time * FramesPerSecond) % Sprites.Length;
+			if (!IsPlaying || !IsContainsSprites)
+				return;
 
-				image.sprite = Sprites[index];
+			_frameTimer += Time.deltaTime;
+			float timePerFrame = 1f / FramesPerSecond;
+
+			if (_frameTimer >= timePerFrame)
+			{
+				_frameTimer -= timePerFrame;
+
+				_currentFrameIndex = (_currentFrameIndex + 1) % sprites.Length;
+
+				image.sprite = sprites[_currentFrameIndex];
 			}
 		}
 
-		public void SetGif(Sprite[] sprites)
+		public void SetGif(Sprite[] newSprites)
 		{
-			this.sprites = sprites;
+			sprites = newSprites;
 
-			Reset();
+			ResetAnimation();
 		}
 
 		public void Play()
 		{
 			if (!IsContainsSprites)
-				throw new InvalidOperationException("Unable to play gif animation without sprites");
+			{
+				Debug.LogWarning("Unable to play gif animation without sprites.", this);
+
+				return;
+			}
 
 			IsPlaying = true;
 		}
@@ -73,14 +99,13 @@ namespace Evolutex.Evolunity.Components.UI
 		{
 			if (!IsPlaying)
 			{
-				Debug.LogWarning("Trying to stop a gif animation that is not playing");
+				Debug.LogWarning("Trying to stop a gif animation that is not playing.", this);
 
 				return;
 			}
 
 			IsPlaying = false;
-
-			Reset();
+			ResetAnimation();
 		}
 
 		private void InitializeIfRequired()
@@ -89,9 +114,13 @@ namespace Evolutex.Evolunity.Components.UI
 				image = GetComponent<Image>();
 		}
 
-		private void Reset()
+		private void ResetAnimation()
 		{
-			image.sprite = Sprites[0];
+			_frameTimer = 0f;
+			_currentFrameIndex = 0;
+
+			if (IsContainsSprites && IsInitialized)
+				image.sprite = sprites[0];
 		}
 	}
 }
