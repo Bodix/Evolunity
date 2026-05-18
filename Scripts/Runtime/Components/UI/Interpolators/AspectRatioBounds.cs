0e57254227a36057c4644cb2c2d44b2d2a6c02e8
Bodix <bodix321@gmail.com>
2026-05-18 05:02:34 +0300

    Add aspect ration bounds

--- Scripts/Runtime/Components/UI/Interpolators/AspectRatioBounds.cs
+++ Scripts/Runtime/Components/UI/Interpolators/AspectRatioBounds.cs
@@ -5,13 +5,13 @@ namespace Bodix.Evolunity.Components.UI
 	/// <summary>
 	/// Contains screen aspect ratio boundaries. Used primarily for UI interpolation.
 	/// </summary>
-	[CreateAssetMenu(fileName = "AspectRatioBounds", menuName = "UI/Aspect Ratio Bounds")]
+	// [CreateAssetMenu(fileName = nameof(AspectRatioBounds), menuName = "UI/Aspect Ratio Bounds")]
 	public class AspectRatioBounds : ScriptableObject
 	{
-		[Tooltip("The widest supported aspect ratio.")]
+		[Tooltip("The widest supported aspect ratio. 2520:1080 (7:3) = 2.333333")]
 		public float WidestRatio = 7f / 3f;
 
-		[Tooltip("The narrowest supported aspect ratio.")]
+		[Tooltip("The narrowest supported aspect ratio. 2048:1536 (4:3) = 1.333333")]
 		public float NarrowestRatio = 4f / 3f;
 	}
 }
