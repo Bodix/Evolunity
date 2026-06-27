0cbda30f76153e257b27e67cbbb350fead628073
Bodix <bodix321@gmail.com>
2026-06-27 08:04:30 +0300

    Add `AssetDatabaseHelper`

---
+++ Scripts/Editor/Utilities/AssetDatabaseHelper.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+
+namespace Bodix.Evolunity.Editor.Utilities
+{
+	public static class AssetDatabaseHelper
+	{
+		public static T FindFirstAssetByType<T>() where T : UnityEngine.Object
+		{
+			string typeName = typeof(T).Name;
+			string[] guids = AssetDatabase.FindAssets("t:" + typeName);
+
+			return guids.Length == 0
+				? null
+				: AssetDatabase.LoadAssetAtPath<T>(AssetDatabase.GUIDToAssetPath(guids[0]));
+		}
+	}
+}
