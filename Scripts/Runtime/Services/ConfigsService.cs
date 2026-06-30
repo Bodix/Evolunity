6a8ad768ee1e1e2b7979f41d72b9f16429eabfe9
Bodix <bodix321@gmail.com>
2026-06-30 12:46:35 +0300

    Add useful services

---
+++ Scripts/Runtime/Services/ConfigsService.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Bodix.Evolunity.Collections;
+using UnityEngine;
+
+namespace Bodix.Evolunity.Services
+{
+	public class ConfigService
+	{
+		private readonly Dictionary<string, DataAsset> _configs;
+
+		public ConfigService(Dictionary<string, DataAsset> configs)
+		{
+			_configs = configs;
+		}
+
+		public T GetConfig<T>(string id) where T : DataAsset
+		{
+			if (string.IsNullOrEmpty(id))
+			{
+				Debug.LogError($"[{nameof(ConfigService)}] Requested config with an empty ID.");
+				return null;
+			}
+
+			if (_configs.TryGetValue(id, out DataAsset asset))
+			{
+				if (asset is T typedAsset)
+					return typedAsset;
+
+				Debug.LogError($"[{nameof(ConfigService)}] Config '{id}' was found, but it is not of type {typeof(T).Name}.");
+				return null;
+			}
+
+			Debug.LogError($"[{nameof(ConfigService)}] Config with ID '{id}' not found in the registry.");
+			return null;
+		}
+	}
+}
---
+++ Scripts/Runtime/Services/PauseService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace Bodix.Evolunity.Services
+{
+	public sealed class PauseService
+	{
+		public bool IsPaused { get; private set; }
+
+		public event Action<bool> PauseStateChanged;
+
+		public void SetPause(bool isPaused)
+		{
+			if (IsPaused == isPaused)
+				return;
+
+			Time.timeScale = isPaused ? 0f : 1f;
+			IsPaused = isPaused;
+
+			PauseStateChanged?.Invoke(isPaused);
+		}
+
+		public void TogglePause()
+		{
+			SetPause(!IsPaused);
+		}
+	}
+}
