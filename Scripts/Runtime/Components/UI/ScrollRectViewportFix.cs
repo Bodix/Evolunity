9c259699344903888a650bc6993622047733f6df
Bodix <bodix321@gmail.com>
2026-07-05 20:49:31 +0300

    Add `ScrollRectViewportFix`

---
+++ Scripts/Runtime/Components/UI/ScrollRectViewportFix.cs
@@ -0,0 +1,39 @@
+﻿// Evolunity for Unity
+// Copyright © 2020 Bogdan Nikolayev <bodix321@gmail.com>
+// All Rights Reserved
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Bodix.Evolunity.Components.UI
+{
+	[RequireComponent(typeof(ScrollRect))]
+	public class ScrollRectViewportFix : MonoBehaviour
+	{
+		private ScrollRect _scrollRect;
+
+		private void Awake()
+		{
+			_scrollRect = GetComponent<ScrollRect>();
+		}
+
+		private void Start()
+		{
+			RestoreViewportLayout();
+		}
+
+		private void RestoreViewportLayout()
+		{
+			if (_scrollRect.viewport == null)
+				return;
+
+			RectTransform viewport = _scrollRect.viewport;
+
+			viewport.anchorMin = Vector2.zero;
+			viewport.anchorMax = Vector2.one;
+
+			viewport.offsetMin = Vector2.zero;
+			viewport.offsetMax = Vector2.zero;
+		}
+	}
+}
