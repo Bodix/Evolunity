0dd85bfbe83db5253088bbc692b78a2470828d37
Bodix <bodix321@gmail.com>
2026-07-11 17:24:31 +0300

    Add interpolator

---
+++ Scripts/Runtime/Components/UI/Interpolators/LayoutGroups/LayoutData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Bodix.Evolunity.Components.UI
+{
+	[Serializable]
+	public class LayoutData
+	{
+		public int PaddingLeft;
+		public int PaddingRight;
+		public int PaddingTop;
+		public int PaddingBottom;
+		public float Spacing;
+
+		public LayoutData()
+		{
+		}
+
+		public LayoutData(int left, int right, int top, int bottom, float spacing)
+		{
+			PaddingLeft = left;
+			PaddingRight = right;
+			PaddingTop = top;
+			PaddingBottom = bottom;
+			Spacing = spacing;
+		}
+	}
+}
---
+++ Scripts/Runtime/Components/UI/Interpolators/LayoutGroups/LayoutInterpolator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Bodix.Evolunity.Components.UI
+{
+	public class LayoutInterpolator : ComponentRatioInterpolator<HorizontalOrVerticalLayoutGroup, LayoutData>
+	{
+		protected override LayoutData InterpolateData(LayoutData widest, LayoutData narrowest, float t)
+		{
+			int left = Mathf.RoundToInt(Mathf.Lerp(widest.PaddingLeft, narrowest.PaddingLeft, t));
+			int right = Mathf.RoundToInt(Mathf.Lerp(widest.PaddingRight, narrowest.PaddingRight, t));
+			int top = Mathf.RoundToInt(Mathf.Lerp(widest.PaddingTop, narrowest.PaddingTop, t));
+			int bottom = Mathf.RoundToInt(Mathf.Lerp(widest.PaddingBottom, narrowest.PaddingBottom, t));
+			float spacing = Mathf.Lerp(widest.Spacing, narrowest.Spacing, t);
+
+			return new LayoutData(left, right, top, bottom, spacing);
+		}
+
+		protected override LayoutData ExtractDataFromTarget()
+		{
+			return new LayoutData(
+				Target.padding.left,
+				Target.padding.right,
+				Target.padding.top,
+				Target.padding.bottom,
+				Target.spacing
+			);
+		}
+
+		protected override void ApplyDataToTarget(LayoutData data)
+		{
+			Target.padding = new RectOffset(data.PaddingLeft, data.PaddingRight, data.PaddingTop, data.PaddingBottom);
+			Target.spacing = data.Spacing;
+		}
+	}
+}
