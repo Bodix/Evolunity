bd42f11e6adabd64cc240ad99dfbbfa275dbdacd
Bodix <bodix321@gmail.com>
2026-07-16 11:34:36 +0300

    Refactoring

--- Scripts/Runtime/Utilities/AssetDatabaseHelper.cs
+++ Scripts/Runtime/Utilities/AssetDatabaseHelper.cs
@@ -6,12 +6,17 @@ namespace Bodix.Evolunity.Utilities
 	{
 		public static T FindFirstAssetByType<T>() where T : UnityEngine.Object
 		{
+#if UNITY_EDITOR
 			string typeName = typeof(T).Name;
 			string[] guids = AssetDatabase.FindAssets("t:" + typeName);
 
 			return guids.Length == 0
 				? null
 				: AssetDatabase.LoadAssetAtPath<T>(AssetDatabase.GUIDToAssetPath(guids[0]));
+#else
+			UnityEngine.Debug.LogError($"Trying to use {nameof(AssetDatabaseHelper)} not in Unity Editor");
+			return null;
+#endif
 		}
 	}
 }
