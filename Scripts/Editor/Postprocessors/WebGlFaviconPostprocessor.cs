7cfec8f0e72748d0307288dc51cdcd99fc9d465f
Bodix <bodix321@gmail.com>
2026-07-18 10:43:58 +0300

    Add WebGL postprocessor

---
+++ Scripts/Editor/Postprocessors/WebGlFaviconPostprocessor.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+namespace Bodix.Evolunity.Editor.Postprocessors
+{
+	public class WebGlFaviconPostprocessor : IPostprocessBuildWithReport
+	{
+		public int callbackOrder => 1;
+
+		public void OnPostprocessBuild(BuildReport report)
+		{
+			if (report.summary.platform != BuildTarget.WebGL)
+				return;
+
+			WebGlPostprocessorsSettings settings = WebGlPostprocessorsSettings.Load();
+			string sourceIconPath = settings.FaviconPath;
+
+			if (string.IsNullOrEmpty(sourceIconPath))
+				return;
+
+			if (!Path.IsPathRooted(sourceIconPath))
+				sourceIconPath = Path.Combine(Directory.GetCurrentDirectory(), sourceIconPath);
+
+			string targetIconPath = Path.Combine(report.summary.outputPath, "TemplateData", "favicon.ico");
+
+			if (File.Exists(sourceIconPath) && Directory.Exists(Path.Combine(report.summary.outputPath, "TemplateData")))
+				File.Copy(sourceIconPath, targetIconPath, true);
+			else if (!File.Exists(sourceIconPath))
+				Debug.LogWarning($"Favicon NOT found at path: {sourceIconPath}. Default icon will be used.");
+		}
+	}
+}
---
+++ Scripts/Editor/Postprocessors/WebGlPostprocessorsSettings.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace Bodix.Evolunity.Editor.Postprocessors
+{
+	[System.Serializable]
+	public class WebGlPostprocessorsSettings
+	{
+		public bool NicifyTitle = true;
+		public string FaviconPath = "Assets/favicon.ico";
+
+		private static string SettingsPath => "ProjectSettings/WebGLPostprocessorsSettings.json";
+
+		public static WebGlPostprocessorsSettings Load()
+		{
+			if (File.Exists(SettingsPath))
+			{
+				string json = File.ReadAllText(SettingsPath);
+
+				return JsonUtility.FromJson<WebGlPostprocessorsSettings>(json);
+			}
+
+			return new WebGlPostprocessorsSettings();
+		}
+
+		public void Save()
+		{
+			string json = JsonUtility.ToJson(this, true);
+
+			File.WriteAllText(SettingsPath, json);
+		}
+	}
+}
