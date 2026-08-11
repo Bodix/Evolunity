0648454c8641d872641d72cf51fbf27b198236f4
Bodix <bodix321@gmail.com>
2026-08-11 04:18:55 +0300

    Add dialogs service

---
+++ Scripts/Runtime/Services/DialogService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Bodix.Evolunity.Components.UI;
+using UnityEngine;
+
+namespace Bodix.Evolunity.Services
+{
+	public class DialogService : MonoBehaviour
+	{
+		[SerializeField]
+		protected UiConfirmationDialog confirmationDialogPrefab;
+		[SerializeField]
+		protected Transform dialogsParent;
+
+		public void ShowConfirmationDialog(UiConfirmationDialogPayload payload,
+			Action<UiConfirmationDialog.Result> onResult)
+		{
+			UiConfirmationDialog dialog = Instantiate(confirmationDialogPrefab, dialogsParent);
+
+			dialog.Show(payload, result =>
+			{
+				onResult?.Invoke(result);
+
+				Destroy(dialog.gameObject);
+			});
+		}
+	}
+}
