c238c48bab4ebdd061a62e8968b0319573a2f849
Bodix <bodix321@gmail.com>
2026-08-12 11:59:02 +0300

    Improve button

---
+++ Scripts/Runtime/Components/UI/Elements/Buttons/ObservableButton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Bodix.Evolunity.Components.UI
+{
+	/// <summary>
+	/// It simply adds an event when the interactivity state (<see cref="Button.interactable"/>) changes.
+	/// </summary>
+	[AddComponentMenu("")]
+	internal class ObservableButton : Button
+	{
+		public event Action<bool> InteractabilityChanged;
+
+		private bool _wasInteractable = true;
+
+		protected override void Awake()
+		{
+			base.Awake();
+
+			_wasInteractable = interactable;
+		}
+
+		protected override void DoStateTransition(SelectionState state, bool instant)
+		{
+			base.DoStateTransition(state, instant);
+
+			if (interactable != _wasInteractable)
+			{
+				_wasInteractable = interactable;
+
+				InteractabilityChanged?.Invoke(_wasInteractable);
+			}
+		}
+	}
+}
--- Scripts/Runtime/Components/UI/Elements/Buttons/UiButton.cs
+++ Scripts/Runtime/Components/UI/Elements/Buttons/UiButton.cs
@@ -2,6 +2,7 @@
 // Copyright © 2020 Bogdan Nikolayev <bodix321@gmail.com>
 // All Rights Reserved
 
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -15,7 +16,44 @@ namespace Bodix.Evolunity.Components.UI
 		[SerializeField]
 		protected Image background;
 
+		public event Action<bool> InteractabilityChanged;
+
 		public Button Button => button;
 		public Image Background => background;
+		public bool IsInteractable
+		{
+			get => button.interactable;
+			set
+			{
+				if (button.interactable != value)
+				{
+					button.interactable = value;
+
+					// If it's an ObservableButton, the event will fire automatically.
+					// If not, we fire it manually here to ensure backward compatibility.
+					if (!(button is ObservableButton))
+						InvokeInteractabilityChangedEvent(value);
+				}
+			}
+		}
+
+		protected override void Awake()
+		{
+			base.Awake();
+
+			if (button is ObservableButton observableButton)
+				observableButton.InteractabilityChanged += InvokeInteractabilityChangedEvent;
+		}
+
+		protected virtual void OnDestroy()
+		{
+			if (button is ObservableButton observableButton)
+				observableButton.InteractabilityChanged -= InvokeInteractabilityChangedEvent;
+		}
+
+		private void InvokeInteractabilityChangedEvent(bool isInteractable)
+		{
+			InteractabilityChanged?.Invoke(isInteractable);
+		}
 	}
 }
