47f4755a41f4bc6ac0a2dfd0f2ce2efcc6c811c9
Bodix <bodix321@gmail.com>
2026-08-13 05:26:20 +0300

    Add component menu items

---
+++ Scripts/Editor/MenuItems/ComponentMenuItems.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+// https://forum.unity.com/threads/expand-collapse-all-components-in-a-gameobject-refresh-issue.517866/
+namespace Bodix.Evolunity.Editor
+{
+	public static class ComponentMenuItems
+	{
+		[MenuItem("CONTEXT/Component/Collapse All")]
+		public static void CollapseAll(MenuCommand command)
+		{
+			GameObject gameObject = (command.context as Component).gameObject;
+			Component[] components = gameObject.GetComponents<Component>();
+			foreach (Component component in components)
+				InternalEditorUtility.SetIsInspectorExpanded(component, false);
+
+			ActiveEditorTracker.sharedTracker.ForceRebuild();
+		}
+
+		[MenuItem("CONTEXT/Component/Expand All")]
+		public static void ExpandAll(MenuCommand command)
+		{
+			GameObject gameObject = (command.context as Component).gameObject;
+			Component[] components = gameObject.GetComponents<Component>();
+			foreach (Component component in components)
+				InternalEditorUtility.SetIsInspectorExpanded(component, true);
+
+			ActiveEditorTracker.sharedTracker.ForceRebuild();
+		}
+	}
+}
