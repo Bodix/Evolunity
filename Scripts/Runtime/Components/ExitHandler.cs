a176cb1a9c50ffd64f3235f9a17c197ce96e0c24
Bodix <bodix321@gmail.com>
2026-08-15 11:44:48 +0300

    Add exit handler

---
+++ Scripts/Runtime/Components/ExitHandler.cs
@@ -0,0 +1,33 @@
+﻿using Bodix.Evolunity.Components.UI;
+using Bodix.Evolunity.Services;
+using UnityEngine;
+
+namespace Bodix.Evolunity.Components
+{
+	public abstract class ExitHandler : MonoBehaviour, IBackNavigationHandler
+	{
+		protected abstract UiDialogService UiDialogService { get; }
+		protected virtual string TitleText { get; } = "Exit";
+		protected virtual string MessageText { get; } = "Are you sure you want to exit?";
+		protected virtual string AcceptButtonText { get; set; } = "Yes, exit";
+		protected virtual string DeclineButtonText { get; set; } = "No, stay";
+
+		public bool OnBackPressed()
+		{
+			UiDialogService.ShowConfirmationDialog(new UiConfirmationDialogPayload
+			{
+				HideOnBackgroundClick = true,
+				Title = TitleText,
+				Message = MessageText,
+				AcceptButtonText = AcceptButtonText,
+				DeclineButtonText = DeclineButtonText,
+			}, result =>
+			{
+				if (result == UiConfirmationDialog.Result.Accept)
+					Application.Quit();
+			});
+
+			return true;
+		}
+	}
+}
