927a9c4887b3f709ce0b47da2935d02f3ad699f4
Bodix <bodix321@gmail.com>
2026-09-06 12:28:47 +0300

    Refactoring

--- Scripts/Runtime/Components/UI/Elements/UiProgressBar.cs
+++ Scripts/Runtime/Components/UI/Elements/UiProgressBar.cs
@@ -30,7 +30,7 @@ namespace Bodix.Evolunity.Components.UI
 			}
 		}
 
-		public void UpdateFillImage()
+		public virtual void UpdateFillImage()
 		{
 			// IMPORTANT: Using anchors instead of "fillAmount" to add possibility to work with Sliced image type.
 			fillImage.rectTransform.anchorMax = new Vector2(_normalizedValue, fillImage.rectTransform.anchorMax.y);
