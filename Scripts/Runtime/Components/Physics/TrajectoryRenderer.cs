8caed0d63f02fa4852175101c2399ec66fc5cf95
Bodix <bodix321@gmail.com>
2026-09-07 13:49:40 +0300

    Improve `TrajectoryRenderer`

--- Scripts/Runtime/Components/Physics/TrajectoryRenderer.cs
+++ Scripts/Runtime/Components/Physics/TrajectoryRenderer.cs
@@ -22,17 +22,74 @@ namespace Bodix.Evolunity.Components
 
 		private LineRenderer _lineRenderer;
 		private Vector3[] _points;
+		private GradientColorKey[] _originalColorKeys;
+		private GradientAlphaKey[] _originalAlphaKeys;
+		private bool _hasColorOverride;
+		private Color _overrideColor;
 
 		private void Awake()
 		{
-			if (!_lineRenderer)
-				_lineRenderer = GetComponent<LineRenderer>();
-
-			_points = new Vector3[_segmentCount];
+			EnsureInitialized();
 		}
 
 		public void DrawTrajectory(Vector3 startPoint, Vector3 initialVelocity)
 		{
+			ApplyTrajectory(startPoint, initialVelocity);
+			ResetColor();
+		}
+
+		public void DrawTrajectory(Vector3 startPoint, Vector3 initialVelocity, Color color)
+		{
+			ApplyTrajectory(startPoint, initialVelocity);
+			SetColor(color);
+		}
+
+		public void SetColor(Color color)
+		{
+			EnsureInitialized();
+
+			if (_hasColorOverride && _overrideColor == color)
+				return;
+
+			GradientColorKey[] colorKeys = new GradientColorKey[_originalColorKeys.Length];
+			for (int i = 0; i < _originalColorKeys.Length; i++)
+			{
+				GradientColorKey original = _originalColorKeys[i];
+				colorKeys[i] = new GradientColorKey(color, original.time);
+			}
+
+			Gradient gradient = new Gradient();
+			gradient.SetKeys(colorKeys, _originalAlphaKeys);
+			_lineRenderer.colorGradient = gradient;
+			_hasColorOverride = true;
+			_overrideColor = color;
+		}
+
+		public void ResetColor()
+		{
+			if (!_hasColorOverride)
+				return;
+
+			EnsureInitialized();
+
+			Gradient gradient = new Gradient();
+			gradient.SetKeys(_originalColorKeys, _originalAlphaKeys);
+			_lineRenderer.colorGradient = gradient;
+			_hasColorOverride = false;
+		}
+
+		public void HideTrajectory()
+		{
+			gameObject.SetActive(false);
+			ResetColor();
+
+			if (_hitCrosshair)
+				_hitCrosshair.SetActive(false);
+		}
+
+		private void ApplyTrajectory(Vector3 startPoint, Vector3 initialVelocity)
+		{
+			EnsureInitialized();
 			gameObject.SetActive(true);
 
 			Vector3 gravity = Physics.gravity;
@@ -73,12 +130,20 @@ namespace Bodix.Evolunity.Components
 			_lineRenderer.SetPositions(_points);
 		}
 
-		public void HideTrajectory()
+		private void EnsureInitialized()
 		{
-			gameObject.SetActive(false);
+			if (!_lineRenderer)
+				_lineRenderer = GetComponent<LineRenderer>();
 
-			if (_hitCrosshair)
-				_hitCrosshair.SetActive(false);
+			if (_points == null || _points.Length != _segmentCount)
+				_points = new Vector3[_segmentCount];
+
+			if (_originalColorKeys == null)
+			{
+				Gradient gradient = _lineRenderer.colorGradient;
+				_originalColorKeys = gradient.colorKeys;
+				_originalAlphaKeys = gradient.alphaKeys;
+			}
 		}
 	}
 }
