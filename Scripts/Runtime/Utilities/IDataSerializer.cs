d2ae3631739c7dd9f389b8782866e988bbddfc41
Bodix <bodix321@gmail.com>
2026-09-14 21:59:31 +0300

    Refactoring

--- Scripts/Runtime/Utilities/IDataSerializer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Bodix.Evolunity.Utilities
-{
-	public interface IDataSerializer
-	{
-		string Extension { get; }
-		void Serialize<T>(T data, string filePath);
-		T Deserialize<T>(string filePath);
-	}
-}
